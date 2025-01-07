d4a5feb6756ba914f00f5eb0c0849b066d9fa3e5
LONEWOLF143 <twaha2030@gmail.com>
2025-01-07 11:56:11 +0300

    O(log n) - Logarithmic Time Complexity

--- index.js
+++ index.js
@@ -42,19 +42,27 @@
 // O(n^2) - Quadratic Time Complexity
 // indicates that algorithm's execution grows quadratically with the input size data (represented by n)
 
-function findPairs(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      console.log(`pair: ${arr[i]}, ${arr[j]}`);
-    }
-  }
+// function findPairs(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       console.log(`pair: ${arr[i]}, ${arr[j]}`);
+//     }
+//   }
 
-  //O(n)
-  for (let q = 0; q < 5; q++) {
-    console.log("----------", q);
-  }
-}
+//   //O(n)
+//   for (let q = 0; q < 5; q++) {
+//     console.log("----------", q);
+//   }
+// }
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-findPairs(numbers); // O(n^2) - Quadratic Time Complexity
+// findPairs(numbers); // O(n^2) - Quadratic Time Complexity
+
+// O(log n) - Logarithmic Time Complexity 
+// refers to an algorithm's runtime that grows logarithmically
+// with the size of the input(reprensented by n). in simple terms,
+// as the input size increases, the time it takes for the algorithm
+// to run increases slowly. 
+
+
