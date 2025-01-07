a64ed1bb3d2cf8ca4006e6067ba8126180139d09
LONEWOLF143 <twaha2030@gmail.com>
2025-01-07 16:15:17 +0300

    custom push method

---
+++ DS-arrays.js
@@ -0,0 +1,25 @@
+// Arrays
+// Data structures array is a an ordered collection of elements that can be accessed using a numerical index
+// const boolArr = [true, false];
+// const mixed = ['a', 2, true, undefined, null, { a:'a',}, ["b"]];
+// console.log(mixed);
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+
+    }
+}
+
+const myNewArray = new MyArray();
+
+myNewArray.push('apple');
+myNewArray.push('banana');
+myNewArray.push('mango');
+console.log(myNewArray.data);
