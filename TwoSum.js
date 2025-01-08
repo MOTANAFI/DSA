b7f215017e6fe46f1b3ad06517a4ba7bf7aac6df
LONEWOLF143 <twaha2030@gmail.com>
2025-01-08 16:57:55 +0300

    two sum to be cont

---
+++ TwoSum.js
@@ -0,0 +1,9 @@
+// Two sum
+// imaging you have a list of numbers and a target number.Your job
+//is to find two numbers in that list that add up to the target number.
+// You also need to tell which position (or inexes) those two numbers ar at in the list
+
+// example 
+//if the list is [2,7,11,15] and the target is 9,
+// the answer would be [0,1] because 2 (at index 0) plus 7 (at index 1) equals 9;
+
