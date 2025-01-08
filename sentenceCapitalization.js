6762600c13f812fb6e3719fb33633cecd9ed9098
LONEWOLF143 <twaha2030@gmail.com>
2025-01-08 11:44:25 +0300

    capitalize words

--- sentenceCapitalization.js
+++ sentenceCapitalization.js
@@ -9,17 +9,32 @@
 //     return words.join(' ');
 
 // }
-// console.log(sentenceCapitalization('hello world'));  
+// console.log(sentenceCapitalization('hello world'));
 
 // using for of loop
 
-const sentenceCapitalization = str => {
-    const words = str.toLowerCase().split(' ');
-    capitalizedWords = [];
-    for (let word of words) {
-        capitalizedWords.push(word[0].toUpperCase() + word.slice(1));
-    }
-    return capitalizedWords .join(' ');
+const sentenceCapitalization = (str) => {
+  // make the string to lowercase and split
+  const words = str.toLowerCase().split(" ");
+  // create an empty array to store the capitalized words
+  capitalizedWords = [];
+  // loop through the words array
+  for (let word of words) {
+    // capitalize the first letter of each word
+    capitalizedWords.push(word[0].toUpperCase() + word.slice(1));
+  }
+  // return the capitalized words array
+  return capitalizedWords.join(" ");
+};
+
+console.log(sentenceCapitalization("hello world")); // Hello World
+
+// usign map method 
+
+const capitalize = (str) => {
+    return str.toLowerCase().split(' ').map(word => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
 }
 
-console.log(sentenceCapitalization('hello world'));  // Hello World
+console.log(capitalize('hello world')); // Hello World
