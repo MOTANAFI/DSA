fcb3bec65c145e02c050277a7640814ebf8d2ed3
LONEWOLF143 <twaha2030@gmail.com>
2025-01-14 14:44:49 +0300

    created linked list and node list

---
+++ LinkedList.js
@@ -0,0 +1,23 @@
+// Linked List
+// A linked list is a linear data structure where elemenst,
+// called nodes, are not stored congtiguously in memory.
+// instaed, each node contains data and a reference, or link,
+// to the next node in the sequence.
+
+class Node {
+  constructor(value) {
+    this.head = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head
+
+    this.length = 1
+  }
+}
+
+
