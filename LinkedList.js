b97ae4c6835929143c040e57eb6b93132947c386
LONEWOLF143 <twaha2030@gmail.com>
2025-01-14 15:06:49 +0300

    linked list pop method

--- LinkedList.js
+++ LinkedList.js
@@ -14,13 +14,13 @@ class Node {
 class LinkedList {
   constructor(value) {
     this.head = new Node(value);
-    this.tail = this.head
+    this.tail = this.head;
 
-    this.length = 1
+    this.length = 1;
   }
   push(value) {
     if (!this.head) {
-      this.head = newNode
+      this.head = newNode;
       this.tail = newNode;
     }
     const newNode = new Node(value);
@@ -28,9 +28,32 @@ class LinkedList {
     this.tail = newNode;
     this.length++;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let pre = this.head;
+
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp;
+  }
 }
 
 const myLinkedList = new LinkedList(1);
-myLinkedList.push(10)
+myLinkedList.push(10);
+myLinkedList.pop();
 
-console.log(myLinkedList)
+console.log(myLinkedList);
