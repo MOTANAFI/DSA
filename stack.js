4be6139221d8b242d6224b942aa3764b5863d63e
LONEWOLF143 <twaha2030@gmail.com>
2025-01-16 15:11:02 +0300

    push method in the stack

--- stack.js
+++ stack.js
@@ -13,10 +13,26 @@ class Stack {
   constructor(value) {
     const newNode = new Node(value);
     this.first = newNode;
-    this.lenght = 1;
+    this.length = 1;
+  }
+
+  push(value){
+   const newNode =  new Node(value);
+   if(this.lenght === 0) {
+    this.first = newNode;
+   }
+
+   newNode.next = this.first;
+   this.first = newNode;
+   this.length++
+
+   return this
+
   }
 }
 
 let theStack = new Stack(0);
+theStack.push(1)
+theStack.push(2)
 
 console.log(theStack);
