cb6ba0ab4b669828e8bc94aecccef30e4d5a3b6f
LONEWOLF143 <twaha2030@gmail.com>
2025-01-23 17:38:48 +0300

    hashtable method- getvalues and keys

--- HashTable.js
+++ HashTable.js
@@ -26,7 +26,7 @@ class HashTable {
     let sum = 0;
     const PRIME_NUMBER = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
-      const charCode = key.charCodeAt(0) - 96;
+      const charCode = key.charCodeAt(i) - 96;
       sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length;
     }
     return sum;
@@ -34,27 +34,54 @@ class HashTable {
 
   set(key, value) {
     const index = this._hashFunction(key);
-   
-    // console.log(!this.keyMap[index]) 
-    if(!this.keyMap[index]) this.keyMap[index] = [];
-   
-    this.keyMap[index] = [this.keyMap[index], [key, value]]; 
-    return this
 
+    // console.log(!this.keyMap[index])
+    if (!this.keyMap[index]) this.keyMap[index] = [];
+
+    this.keyMap[index].push([key, value]);
+    return this;
   }
   get(key) {
     const index = this._hashFunction(key);
-    if(this.keyMap[index]){
-      for(let i = 0; i < this.keyMap[index].length; i++){
-        if(this.keyMap[index][i][0] === key){
-          return this.keyMap[index][i][1]
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
         }
       }
     }
-    return undefined
+    return undefined;
+  }
+  getAllKeys() {
+    const keys = [];
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          keys.push(this.keyMap[i][j][0]);
+        }
+      }
+    }
+    return keys;
+  }
+  getAllValue() {
+    const values = [];
+
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          values.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+    return values;
   }
 }
 
 const phoneBook = new HashTable();
-console.log(phoneBook.set("john", "555-333-444"))
-console.log(phoneBook.get("john"));
+phoneBook.set("john", "555-333-444");
+phoneBook.set("lakeer", "555-333-444");
+phoneBook.set("jonny", "555-333-444");
+phoneBook.set("hanu", '9848-484-498');
+console.log(phoneBook.getAllKeys());
+console.log(phoneBook.getAllValue());
