4ee6b13f355b8293957ffe6235bb1dc66d9f00a7
LONEWOLF143 <twaha2030@gmail.com>
2025-01-23 18:25:02 +0300

    trees in dsa

---
+++ Tree.js
@@ -0,0 +1,4 @@
+// TREE
+// A DS is a fundamental hierarchical data structure.
+// it represents a collection of elements (data) called nodes, connected by edges (links)
+//forming a tree-like structure
