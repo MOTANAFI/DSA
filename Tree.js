e620847fe78102e03eb28065f49a2c2cfaafedbd
LONEWOLF143 <twaha2030@gmail.com>
2025-01-24 09:58:19 +0300

    trees

--- Tree.js
+++ Tree.js
@@ -2,3 +2,17 @@
 // A DS is a fundamental hierarchical data structure.
 // it represents a collection of elements (data) called nodes, connected by edges (links)
 //forming a tree-like structure
+
+// PERFECT TEE
+// Full + balance
+//BALANCE: All the leaf nodes are the same depth (distance from root).
+// This means the tree is completely filled with no gaps at any level.
+
+
+// COMPLETE TREE
+
+// Almost Full levels: All levels except possibly the last one are 
+// compleletly filled with nodes. 
+// Left Leaning Last Level: The last level is filled with nodes
+// from the left side as much as possible. 
+//There might be empty spaces on the right.
