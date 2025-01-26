834b959000185c4a8448b71682dda664352fe951
LONEWOLF143 <twaha2030@gmail.com>
2025-01-26 10:27:18 +0300

    remove edges in graph

--- Graph.js
+++ Graph.js
@@ -10,42 +10,59 @@
 class Graph {
   constructor() {
     this.adjacencyList = {};
-}
+  }
 
-addVertex(vertex) {
-    if(!this.adjacencyList[vertex]) {
-        this.adjacencyList[vertex] = [];
-        return true;
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
     }
-}
-// connection 
+  }
+  // connection
 
-addEdge(v1, v2) {
-    if(this.adjacencyList[v1] && this.adjacencyList[v2]) {
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-        return true
+  addEdge(v1, v2) {
+    if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+      this.adjacencyList[v1].push(v2);
+      this.adjacencyList[v2].push(v1);
+      return true;
     }
     return false;
-}
-removeEdge(v1, v2) {
-    if(this.adjacencyList[v1] && this.adjacencyList[v2]) {
-        this.adjacencyList[v1] = this.adjacencyList[v2].filter(v => v !== v2);
-        this.adjacencyList[v2] = this.adjacencyList[v1].filter(v => v !== v1);
-        return true
+  }
+  removeEdge(v1, v2) {
+    if (this.adjacencyList[v1] && this.adjacencyList[v2]) {
+      this.adjacencyList[v1] = this.adjacencyList[v2].filter((v) => v !== v2);
+      this.adjacencyList[v2] = this.adjacencyList[v1].filter((v) => v !== v1);
+      return true;
     }
-    return false
-}
-}
+    return false;
+  }
+  removeVertex(v) {
+    if (!this.adjacencyList[v]) return undefined;
 
+    for (let neighbour of this.adjacencyList[v]) {
+      this.adjacencyList[neighbour] = this.adjacencyList[neighbour].filter(
+        (vertex) => vertex !== v
+      );
+    }
+
+    delete this.adjacencyList[v];
+    return this;
+  }
+}
 
 const g = new Graph();
 g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
+g.addVertex("D");
+
 g.addEdge("A", "B");
-g.addEdge("B", "C");
-g.addEdge("C", "A");
-console.log(g)
-g.removeEdge("A", "B");
-console.log(g)
+g.addEdge("A", "C");
+g.addEdge("A", "D");
+g.addEdge("B", "D");
+g.addEdge("C", "D");
+console.log(g);
+// g.removeEdge("A", "B");
+g.removeVertex("D")
+console.log(g);
+
